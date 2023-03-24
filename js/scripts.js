752c7e5c537f117910e027782c33bcd2cb3f2a46
tdietzel <dietzelbiz@outlook.com>
2023-03-24 10:09:24 -0700

    WIP BUGFIX, add js branching & event

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,31 @@
 window.addEventListener("load", function() {
-  const form = document.getElementById("input");
+  const form = document.getElementById('submit');
   form.addEventListener("submit",calcLanguage);
 })
 
 function calcLanguage (event) {
-  event.preventDefault()
+  event.preventDefault();
   const firstChoiceA = window.document.querySelector("input#firstChoiceA").value;
   const firstChoiceB = window.document.querySelector("input#firstChoiceB").value;
+  const secondChoiceA = window.document.querySelector("input#secondChoiceA").value;
+  const secondChoiceB = window.document.querySelector("input#secondChoiceB").value;
+  const thirdChoiceA = window.document.querySelector("input#thirdChoiceA").value;
+  const thirdChoiceB = window.document.querySelector("input#thirdChoiceB").value;
+  const fourthChoiceA = window.document.querySelector("input#fourthChoiceA").value;
+  const fourthChoiceB = window.document.querySelector("input#fourthChoiceB").value;
+  const fifthChoiceA = window.document.querySelector("input#fifthChoiceA").value;
+  const fifthChoiceB = window.document.querySelector("input#fifthChoiceB").value;
 
-  console.log(firstChoiceA);
-  console.log(firstChoiceB);
+  if (document.getElementById('firstChoiceA').checked === true) {
+    javaScript = 1;
+    console.log(javaScript);
+  } elseif (document.getElementById('firstChoiceA').checked === true); {
+    python = 1;
+    console.log(python);
+  }
+
+  if (javaScript >= python) {
+    result="java";
+  }
+  document.getElementById("answer").innerText= result;
 }
